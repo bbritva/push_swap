c86ed3a27336790ca03c3de31791ebf23ce484a2
grvelva <grvelva@student.21-school.ru>
2021-04-10 14:11:42 +0300

    создал пуш_своп, общие утилиты выделил в отдельную папку

--- Makefile
+++ Makefile
@@ -1,18 +1,25 @@
-NAME		=	checker
+CHK			=	checker
+PS			=	push_swap
 LIBFT		=	libft.a
 CCF			=	@gcc $(CFLAGS)
-RM			=	@rm -r
+RM			=	@rm -f
 CFLAGS		=	-Wall -Wextra -Werror -g
 CHECK_PATH	=	checker_dir/
+CHK_SRCS	=	checker.c do_sort.c
 PS_PATH		=	push_swap_dir/
+PS_SRCS		=	push_swap.c
+UTL_PATH	=	utils/
+UTL_SRCS	=	checker_utils.c stack_utils.c stack_manipulators.c
 GNL_PATH	=	gnl_dir/
-CHK_SRCS	=	checker.c checker_utils.c stack_utils.c do_sort.c stack_manipulators.c
 GNL_SRCS	=	get_next_line.c get_next_line_utils.c
-PS_SRCS		=	
-SRCS		=	$(addprefix $(CHECK_PATH), $(CHK_SRCS)) \
-				$(addprefix $(GNL_PATH), $(GNL_SRCS))
-#				$(addprefix $(PS_PATH), $(PS_SRCS))
-OBJS		=	$(SRCS:.c=.o)
+SRCS_CHK	=	$(addprefix $(CHECK_PATH), $(CHK_SRCS)) \
+				$(addprefix $(GNL_PATH), $(GNL_SRCS)) \
+				$(addprefix $(UTL_PATH), $(UTL_SRCS))
+SRCS_PS		=	$(addprefix $(GNL_PATH), $(GNL_SRCS)) \
+				$(addprefix $(UTL_PATH), $(UTL_SRCS)) \
+				$(addprefix $(PS_PATH), $(PS_SRCS))
+OBJS_CHK	=	$(SRCS_CHK:.c=.o)
+OBJS_PS		=	$(SRCS_PS:.c=.o)
 LIB_DIR		=	libft/
 HDR			=	main.h $(GNL_PATH)/get_next_line.h
 CYAN		=	\x1B[36m
@@ -20,26 +27,32 @@ MAGENTA		=	\x1B[35m
 GREEN		=	\033[92m
 ENDCOLOR	=	\x1B[0m
 
-all:		$(NAME)
+all:		$(CHK) $(PS)
 
-
-$(NAME):	$(OBJS) $(HDR)
-			echo $(SRCS)
-			echo $(OBJS)
+$(CHK):		$(OBJS_CHK) $(HDR)
 			@make bonus -C $(LIB_DIR)
 			@mv $(LIB_DIR)$(LIBFT) $(LIBFT)
-			$(CCF) $(OBJS) $(LIBFT) -o $(NAME)
-			@echo "${GREEN}"$(NAME)" is compiled${ENDCOLOR}"
+			$(CCF) $(OBJS_CHK) $(LIBFT) -o $(CHK)
+			@echo "${GREEN}"$(CHK)" is compiled${ENDCOLOR}"
+
+$(PS):		$(OBJS_PS) $(HDR)
+			@make bonus -C $(LIB_DIR)
+			@mv $(LIB_DIR)$(LIBFT) $(LIBFT)
+			$(CCF) $(OBJS_PS) $(LIBFT) -o $(PS)
+			@echo "${GREEN}"$(PS)" is compiled${ENDCOLOR}"
 
 clean:
 			@make clean -C $(LIB_DIR)
-			$(RM) $(OBJS)
+			$(RM) $(OBJS_CHK)
+			$(RM) $(OBJS_PS)
 			@echo objs deleted
 
 fclean:		clean
 			@make fclean -C $(LIB_DIR)
-			$(RM) $(NAME)
-			@echo $(NAME) deleted
+			$(RM) $(CHK)
+			$(RM) $(PS)
+			@echo $(CHK) deleted
+			@echo $(PS) deleted
 
 re:			fclean all
 
---
+++ push_swap_dir/push_swap.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grvelva <grvelva@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/08 18:59:31 by grvelva           #+#    #+#             */
+/*   Updated: 2021/04/09 13:31:10 by grvelva          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../main.h"
+
+void	ft_putstr(char *str)
+{
+	write(1, str, ft_strlen(str));
+}
+
+int 	parse_error(t_stack **stk_a, t_stack **stk_b)
+{
+	ft_stkclear(stk_a);	
+	ft_stkclear(stk_b);
+	ft_putstr("Error\n");
+	return (1);
+}
+
+int		parse_int(int *num, char *str)
+{
+	if (is_int(str))
+	{
+		*num = ft_atoi(str);
+		return (1);
+	}
+	return (0);
+}
+
+int 	get_stack(t_stack **stk, int argc, char *argv[])
+{
+	int		i;
+	int 	num;
+	
+	i = 1;
+	*stk = NULL;
+	while (i < argc)
+	{
+		if (parse_int(&num, argv[i++]))
+			ft_stkadd_back(stk, ft_stknew(num));
+		else
+			return (0);
+	}
+	return (1);
+}
+
+int		main(int argc, char *argv[])
+{
+	t_stack	**stk_a;
+	t_stack	**stk_b;
+	
+	if (argc == 1)
+		return (0);
+	stk_a = (t_stack **)malloc(sizeof(t_stack *));
+	stk_b = (t_stack **)malloc(sizeof(t_stack *));
+	if (stk_a && stk_b)
+	{
+		*stk_b = NULL;
+		if (!get_stack(stk_a, argc, argv) && parse_error(stk_a, stk_b))
+			return (0);
+		ft_stkshow(stk_a);
+		ft_putstr("stack A:\n");
+		ft_stkshow(stk_a);
+		ft_putstr("stack B:\n");
+		ft_stkshow(stk_b);
+		if (is_stk_sorted(stk_a) && !(*stk_b))
+			ft_putstr("OK\n");
+		else
+			ft_putstr("KO\n");
+	}
+	else
+		ft_putstr("Memory allocate error\n");
+}
