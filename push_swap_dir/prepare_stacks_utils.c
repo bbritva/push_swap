9c983c2acd01d4ce787e3d9bf4475d20fec6a429
grvelva <grvelva@student.21-school.ru>
2021-04-25 11:51:49 +0300

    занормил prepare_stacks_utils.c, 4500 для 500 элементов, все работает

--- push_swap_dir/prepare_stacks_utils.c
+++ push_swap_dir/prepare_stacks_utils.c
@@ -1,19 +1,7 @@
 #include "../includes/push_swap.h"
 
-char	*dif_prepare(t_all *all, int num)
+char	*prep_b(t_all *all, int num, int steps, char *ol)
 {
-	char	*ol;
-	int		steps;
-	t_stack	*tmp;
-
-	ol = NULL;
-	steps = 0;
-	tmp = *all->stk_b;
-	while (tmp && tmp->num != num)
-	{
-		steps++;
-		tmp = tmp->next;
-	}
 	if (steps < ft_stksize(all->stk_b) - steps)
 	{
 		while (*all->stk_b && (*all->stk_b)->num != num)
@@ -30,13 +18,13 @@ char	*dif_prepare(t_all *all, int num)
 			ol = gnl_strjoin(ol, RROT_B);
 		}
 	}
-	tmp = *all->stk_a;
-	steps = 0;
-	while (tmp && !check_pos(tmp, num, all->min, all->max))
-	{
-		steps++;
-		tmp = tmp->next;
-	}
+	if (!ol)
+		ol = gnl_strjoin(ol, "");
+	return (ol);
+}
+
+char	*prep_a(t_all *all, int num, int steps, char *ol)
+{
 	if (steps < ft_stksize(all->stk_a) - steps)
 	{
 		while (*all->stk_a && !check_pos(*all->stk_a, num, all->min, all->max))
@@ -53,6 +41,34 @@ char	*dif_prepare(t_all *all, int num)
 			ol = gnl_strjoin(ol, RROT_A);
 		}
 	}
+	if (!ol)
+		ol = gnl_strjoin(ol, "");
+	return (ol);
+}
+
+char	*dif_prepare(t_all *all, int num)
+{
+	char	*ol;
+	int		steps;
+	t_stack	*tmp;
+
+	ol = NULL;
+	steps = 0;
+	tmp = *all->stk_b;
+	while (tmp && tmp->num != num)
+	{
+		steps++;
+		tmp = tmp->next;
+	}
+	ol = prep_b(all, num, steps, ol);
+	tmp = *all->stk_a;
+	steps = 0;
+	while (tmp && !check_pos(tmp, num, all->min, all->max))
+	{
+		steps++;
+		tmp = tmp->next;
+	}
+	ol = prep_a(all, num, steps, ol);
 	if (!ol)
 		ol = gnl_strjoin(ol, "");
 	return (ol);
