5d85a87013c28e66d8634e80b0b0dc5e7f3c0e8b
grvelva <grvelva@student.21-school.ru>
2021-04-25 11:58:19 +0300

    занормил final_rotate.c

--- push_swap_dir/final_rotate.c
+++ push_swap_dir/final_rotate.c
@@ -1,13 +1,33 @@
-
 #include "../includes/push_swap.h"
 
-char 	*final_rotate(t_stack **stk)
+char	*do_rotations(t_stack **stk, int steps, t_stack *min_el, char *ol)
+{
+	if (steps < ft_stksize(stk) - steps)
+	{
+		while ((*stk)->num != min_el->num)
+		{
+			rotate(stk);
+			ol = gnl_strjoin(ol, ROT_A);
+		}
+	}
+	else
+	{
+		while ((*stk)->num != min_el->num)
+		{
+			rev_rotate(stk);
+			ol = gnl_strjoin(ol, RROT_A);
+		}
+	}
+	return (ol);
+}
+
+char	*final_rotate(t_stack **stk)
 {
 	char	*ops_line;
 	t_stack	*min_el;
 	t_stack	*tmp;
-	int 	steps;
-	
+	int		steps;
+
 	ops_line = NULL;
 	min_el = ft_stkmin(stk);
 	tmp = *stk;
@@ -17,23 +37,8 @@ char 	*final_rotate(t_stack **stk)
 		steps++;
 		tmp = tmp->next;
 	}
-	if (steps < ft_stksize(stk) - steps)
-	{
-		while ((*stk)->num != min_el->num)
-		{
-			rotate(stk);
-			ops_line = gnl_strjoin(ops_line, ROT_A);
-		}
-	}
-	else
-	{
-		while ((*stk)->num != min_el->num)
-		{
-			rev_rotate(stk);
-			ops_line = gnl_strjoin(ops_line, RROT_A);
-		}
-	}
-	if (ops_line)
-		return (ops_line);
-	return ("");
+	ops_line = do_rotations(stk, steps, min_el, ops_line);
+	if (!ops_line)
+		ops_line = gnl_strjoin(ops_line, "");
+	return (ops_line);
 }
