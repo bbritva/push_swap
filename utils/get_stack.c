eee349512530aec13b3122df28393ae789c06096
grvelva <grvelva@student.21-school.ru>
2021-04-28 10:03:46 +0300

    сделал обработку строки с интами вместо кучи строк с интами

--- utils/get_stack.c
+++ utils/get_stack.c
@@ -10,6 +10,25 @@ int	parse_int(int *num, char *str)
 	return (0);
 }
 
+int	get_from_string(t_stack **stk, char *str)
+{
+	char	**nums;
+	int		i;
+	int		num;
+
+	nums = ft_split(str, ' ');
+	i = 0;
+	while (nums[i])
+	{
+		if (parse_int(&num, nums[i]) && !has_double(stk, num))
+			ft_stkadd_back(stk, ft_stknew(num));
+		else
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	get_stack(t_stack **stk, int argc, char *argv[])
 {
 	int	i;
@@ -17,6 +36,8 @@ int	get_stack(t_stack **stk, int argc, char *argv[])
 
 	i = 1;
 	*stk = NULL;
+	if (argc == 2)
+		return (get_from_string(stk, argv[1]));
 	while (i < argc)
 	{
 		if (parse_int(&num, argv[i++]) && !has_double(stk, num))
